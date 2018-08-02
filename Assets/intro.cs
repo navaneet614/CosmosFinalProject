bff2da75086f310d971d7c67c191b628703a41f4
navaneet614 <navaneet@gmail.com>
2018-08-02 14:15:39 -0700

    asdasd

--- Assets/intro.cs
+++ Assets/intro.cs
@@ -8,7 +8,8 @@ public class intro : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         //GameObject.Find("Camera").GetComponent<CinemachineBrain>().enabled = false;
-        StartCoroutine("ToggleStuff");
+
+        StartCoroutine("FireWorksToggle1");
 	}
 	
 	// Update is called once per frame
@@ -16,12 +17,40 @@ public class intro : MonoBehaviour {
 
     }
 
-    IEnumerator ToggleStuff()
+    IEnumerator FireWorksToggle1()
     {
         yield return new WaitForSeconds(2);
-        GetComponent<Rigidbody>().useGravity = true;
         GameObject.Find("Effect_07").SetActive(false);
-        transform.Find("pCube11").gameObject.SetActive(true);
+        StartCoroutine("FireWorksToggle2");
+
+    }
+
+
+    IEnumerator FireWorksToggle2()
+    {
+        yield return new WaitForSeconds(2);
+        GameObject.Find("Effect_07 (1)").SetActive(false);
+        StartCoroutine("FireWorksToggle3");
+
+    }
+
+
+    IEnumerator FireWorksToggle3()
+    {
+        yield return new WaitForSeconds(2);
+        GameObject.Find("Effect_07 (2)").SetActive(false);
+        StartCoroutine("ToggleStuff");
+    }
+
+    IEnumerator ToggleStuff()
+    {
+        yield return null;
+        GetComponent<Rigidbody>().useGravity = true;
+        //transform.Find("pCube11").gameObject.SetActive(true);
+        //GameObject.Find("pCube11").SetActive(true);
+        GameObject.Find("pCube11").GetComponent<MeshRenderer>().enabled = true;
+        //GameObject.Find("Effect_04").SetActive(true);
+        //GameObject.Find("Effect_04 (1)").SetActive(true);
         gameObject.GetComponent<Rigidbody>().AddForce(Vector3.down*10, ForceMode.Impulse);
         StartCoroutine("EnableStuff");
     }
@@ -29,14 +58,10 @@ public class intro : MonoBehaviour {
 
     IEnumerator EnableStuff()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(6);
         GameObject.Find("pCube11").GetComponent<TiltScript>().enabled = true;
         gameObject.GetComponent<fireBroUserControl1>().enabled = true;
-        
-        if (GameObject.Find("fireBro2").transform.position.y <= 250)
-        {
-            GameObject.Find("Camera").GetComponent<CinemachineBrain>().enabled = true;
-        }
+        //GameObject.Find("Camera").GetComponent<CinemachineBrain>().enabled = true;
     }
 
 }
