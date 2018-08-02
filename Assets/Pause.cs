92cb4b6699cd27c71667b5befc5d5b5d35be293f
navaneet614 <navaneet@gmail.com>
2018-08-02 10:11:46 -0700

    manasodf

--- Assets/Pause.cs
+++ Assets/Pause.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Pause : MonoBehaviour
 {
+    public Texture pauseScreen;
     private bool paused = false;
 
     void Update()
@@ -23,14 +24,17 @@ public class Pause : MonoBehaviour
             float sHeight = Screen.currentResolution.height;
             if (GUI.Button(new Rect(0, 0, sWidth, sHeight), ""))
                 paused = togglePause();
-            GUIStyle style = new GUIStyle();
-            style.fontSize = 200;
-            style.normal.textColor = Color.white;
-            style.alignment = TextAnchor.UpperCenter;
-            GUI.Label(new Rect(sWidth/2-50, sHeight/2-100, 100, 50), "Game Paused!", style);
-            style.fontSize = 50;
-            style.alignment = TextAnchor.LowerCenter;
-            GUI.Label(new Rect(sWidth / 2 - 50, sHeight / 2 + 100, 100, 50), "Click Anywhere to play. Hit escape to quit.", style);
+            //GUIStyle style = new GUIStyle();
+            //style.fontSize = 200;
+            //style.normal.textColor = Color.white;
+            //style.alignment = TextAnchor.UpperCenter;
+            //GUI.Label(new Rect(sWidth/2-50, sHeight/2-100, 100, 50), "Game Paused!", style);
+            //style.fontSize = 50;
+            //style.alignment = TextAnchor.LowerCenter;
+            //GUI.Label(new Rect(sWidth / 2 - 50, sHeight / 2 + 100, 100, 50), "Click Anywhere to play. Hit escape to quit.", style);
+
+            
+            GUI.DrawTexture(new Rect(sWidth/4, sHeight/2, sWidth/2, sHeight/2), pauseScreen, ScaleMode.StretchToFill);
  
         }
         if (Input.GetKey(KeyCode.Escape) && paused)
