59734c090eb0f62b9495915bb22d11cc891f3849
TD2297 <41814692+TD2297@users.noreply.github.com>
2018-08-02 21:08:53 -0700

    gkuj
    shtsb

---
+++ Assets/SoundSwap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSwap : MonoBehaviour {
+    public Component[] sounds;
+    // Use this for initialization
+    void Start() {
+        sounds = GetComponents(typeof(AudioSource));
+        Debug.Log(sounds[0],sounds[1]);
+    }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+
